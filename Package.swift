b77edfce5d9344428260d8eb2c61632f6f97186c
jk <jinkh93@gmail.com>
2022-08-07 04:23:05 +0200

    rename

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "MOFirebaseLogger",
-    platforms: [.iOS(.v15)],
+    name: "Raigor",
+    platforms: [.iOS(.v15), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MOFirebaseLogger",
-            targets: ["MOFirebaseLogger"]),
+            name: "Raigor",
+            targets: ["Raigor"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MOFirebaseLogger",
+            name: "Raigor",
             dependencies: [.product(name: "FirebaseCrashlytics", package: "firebase-ios-sdk", condition: .when(platforms: [.iOS]))]),
         .testTarget(
-            name: "MOFirebaseLoggerTests",
-            dependencies: ["MOFirebaseLogger"]),
+            name: "RaigorTests",
+            dependencies: ["Raigor"]),
     ]
 )
--- Tests/RaigorTests/LoggerTests.swift
+++ Tests/RaigorTests/LoggerTests.swift
@@ -1,7 +1,7 @@
 import XCTest
-@testable import MOFirebaseLogger
+@testable import Raigor
 
-final class MOFirebaseLoggerTests: XCTestCase {
+final class LoggerTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
