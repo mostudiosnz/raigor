f662eaae7abc9290536fbd4aaa58d0341ed9e87d
jk <jinkh93@gmail.com>
2025-10-02 17:34:59 +0200

    add public log functions

--- Sources/Raigor/Logger.swift
+++ Sources/Raigor/Logger.swift
@@ -6,7 +6,9 @@ import SwiftUI
 
 public protocol Logger: Sendable {
     func log(_ log: String)
+    func log_public(_ log: String)
     func error(_ error: Error)
+    func error_public(_ error: Error)
 }
 
 @propertyWrapper public struct AppLogger: DynamicProperty {
@@ -17,23 +19,39 @@ public protocol Logger: Sendable {
     }
 }
 
+extension Crashlytics: @unchecked @retroactive Sendable {}
+
+/**
+ Logs to both `os.Logger` and `Crashlytics`
+ */
 public actor DefaultLogger: Logger {
     private let osLogger: os.Logger
+    private let fbLogger: Crashlytics
     public init(
         bundle: Bundle = .main,
         fallbackSubsystem: String = "AppLogger",
-        category: String = "Application"
+        category: String = "Application",
+        crashlytics: Crashlytics = .crashlytics()
     ) {
         let subsystem = bundle.bundleIdentifier ?? fallbackSubsystem
         osLogger = os.Logger(subsystem: subsystem, category: category)
+        fbLogger = crashlytics
     }
     nonisolated public func log(_ log: String) {
         osLogger.log("\(log)")
-        Crashlytics.crashlytics().log(log) // stored as a log in an issue
+        fbLogger.log(log)
+    }
+    nonisolated public func log_public(_ log: String) {
+        osLogger.log("\(log)")
+        fbLogger.log(log)
     }
     nonisolated public func error(_ error: Error) {
-        osLogger.error("\(error.description)")
-        Crashlytics.crashlytics().record(error: error, userInfo: createUserInfo()) // stored as a non-fatal issue
+        osLogger.error("\(error.description, privacy: .public)")
+        fbLogger.record(error: error, userInfo: createUserInfo())
+    }
+    nonisolated public func error_public(_ error: any Error) {
+        osLogger.error("\(error.description, privacy: .public)")
+        fbLogger.record(error: error, userInfo: createUserInfo())
     }
     nonisolated func createUserInfo() -> [String: Any] {
         guard #available(iOS 16.0, macOS 13.0, *) else { return [:] }
